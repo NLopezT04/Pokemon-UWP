a8f4ff6408dff8fae82751d3fa3bc1ad2f80d20d
NLopezT04 <Nestor.Lopez2@alu.uclm.es>
2023-12-12 20:26:59 +0100

    App Pokemon con UWP

---
+++ Pokemon/MainPage.xaml.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Core;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// La plantilla de elemento Página en blanco está documentada en https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0xc0a
+
+namespace Pokemon
+{
+    /// <summary>
+    /// Página vacía que se puede usar de forma independiente o a la que se puede navegar dentro de un objeto Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+            SystemNavigationManager.GetForCurrentView().AppViewBackButtonVisibility = AppViewBackButtonVisibility.Visible;
+            SystemNavigationManager.GetForCurrentView().BackRequested += opcionVolver;
+
+            Windows.UI.ViewManagement.ApplicationView.GetForCurrentView().SetPreferredMinSize(new Size(500, 500));
+            Windows.UI.ViewManagement.ApplicationView.GetForCurrentView().VisibleBoundsChanged += MainPage_VisibleBoundsChanged;
+        }
+
+        private void MainPage_VisibleBoundsChanged(Windows.UI.ViewManagement.ApplicationView sender, object args)
+        {
+            var Width = Windows.UI.ViewManagement.ApplicationView.GetForCurrentView().VisibleBounds.Width;
+            if (Width <= 600)
+            {
+                sView.DisplayMode = SplitViewDisplayMode.Overlay;
+            }
+            else
+            {
+                sView.DisplayMode = SplitViewDisplayMode.CompactInline;
+            }
+        }
+
+        private void irPokeDex(object sender, RoutedEventArgs e)
+        {
+            fmMain.Navigate(typeof(PokedexPage));
+        }
+
+        private void irCombate(object sender, RoutedEventArgs e)
+        {
+            fmMain.Navigate(typeof(CombatePage));
+        }
+
+        private void irInicio(object sender, RoutedEventArgs e)
+        {
+            fmMain.Navigate(typeof(MainPage));
+            //fmMain.Navigate(typeof(InicioPage));
+            //fmMain.Navigate(typeof(MainPage(sView(fmMain(ucInicio))));
+            //fmMain.Navigate(typeof(ucInicio));
+        }
+
+        private void irCaptura(object sender, RoutedEventArgs e)
+        {
+            fmMain.Navigate(typeof(CapturaPage));
+        }
+
+        private void opcionVolver(object sender, BackRequestedEventArgs e)
+        {
+            if (fmMain.BackStack.Any())
+            {
+                fmMain.GoBack();
+            }
+        }
+
+        private void btnMenu_Click(object sender, RoutedEventArgs e)
+        {
+            sView.IsPaneOpen = !sView.IsPaneOpen; 
+        }
+
+    }
+}
---
+++ Pokemon/ucCombate.xaml.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Animation;
+using Windows.UI.Xaml.Navigation;
+
+// La plantilla de elemento Control de usuario está documentada en https://go.microsoft.com/fwlink/?LinkId=234236
+
+namespace Pokemon
+{
+    public sealed partial class ucCombate : UserControl
+    {
+        public ucCombate()
+        {
+            this.InitializeComponent();
+            int counter = 0;
+
+            Storyboard sb1 = (Storyboard)this.Resources["sbSaludar"];
+            while (counter < 4)
+            {
+                sb1.AutoReverse = true;
+                sb1.Begin();
+                counter++;
+            }
+        }
+
+        private void ataqueDescanso(object sender, RoutedEventArgs e)
+        {
+            Storyboard sb = (Storyboard)this.Resources["sbDescanso"];
+            tbConsola.Text = "¡ SNORLAX usó DESCANSO ! ";
+            imZzZ.Width = 40; imZzZ.Height = 40; imZzZ.Visibility = Visibility.Visible;
+            sb.Begin();
+            this.pbVida.Value += 50;
+            if (pbVida.Value >= 100)
+            {
+                this.imPocion.Opacity = 0.0;
+            }
+        }
+
+        private void ataqueRizoDefensa(object sender, RoutedEventArgs e)
+        {
+            Storyboard sb = (Storyboard)this.Resources["sbDefensa"];
+            tbConsola.Text = "¡ SNORLAX usó RIZO DEFENSA ! ";
+            ellipse.Visibility = Visibility.Visible;
+            ellipse2.Visibility = Visibility.Visible;
+            sb.Begin();
+            this.pbEscudo.Value += 30;
+        }
+
+        private void ataqueGolpeCuerpo(object sender, RoutedEventArgs e)
+        {
+            tbConsola.Text = "¡ SNORLAX usó AYUDA ! ";
+            var seed = Environment.TickCount;
+            var random = new Random(seed);
+            var value = random.Next(0, 3);
+
+            if (value == 0)
+            {
+                Storyboard sb = (Storyboard)this.Resources["sbDescanso"];
+                tbConsola.Text = "¡ SNORLAX usó DESCANSO ! ";
+                imZzZ.Width = 40; imZzZ.Height = 40; imZzZ.Visibility = Visibility.Visible;
+                sb.Begin();
+                this.pbVida.Value += 50;
+                if (pbVida.Value >= 100)
+                {
+                    this.imPocion.Opacity = 0.0;
+                }
+
+            }
+            else if(value == 1)
+            {
+                Storyboard sb = (Storyboard)this.Resources["sbDefensa"];
+                tbConsola.Text = "¡ SNORLAX usó RIZO DEFENSA ! ";
+                ellipse.Visibility = Visibility.Visible;
+                ellipse2.Visibility = Visibility.Visible;
+                sb.Begin();
+                this.pbEscudo.Value += 30;
+            }
+            else
+            {
+                tbConsola.Text = "¡ SNORLAX usó TAMBOR ! ";
+                path.Visibility = Visibility.Visible;
+                image.Visibility = Visibility.Visible;
+                image_Copy.Visibility = Visibility.Visible;
+                image1.Visibility = Visibility.Visible;
+                image1_Copy1.Visibility = Visibility.Visible;
+                image2.Visibility = Visibility.Visible;
+
+                pbVida.Value /= 2;
+                if (pbVida.Value < 1)
+                {
+                    consolaKO();
+                }
+                else
+                {
+                    Storyboard sb = (Storyboard)this.Resources["sbTambor"];
+                    sb.Begin();
+                }
+                if (pbVida.Value <= 100)
+                {
+                    this.imPocion.Opacity = 100;
+                }
+            }
+        }
+
+        private void ataqueTambor(object sender, RoutedEventArgs e)
+        {
+            tbConsola.Text = "¡ SNORLAX usó TAMBOR ! ";
+            path.Visibility = Visibility.Visible;
+            image.Visibility = Visibility.Visible;
+            image_Copy.Visibility = Visibility.Visible;
+            image1.Visibility = Visibility.Visible;
+            image1_Copy1.Visibility = Visibility.Visible;
+            image2.Visibility = Visibility.Visible;
+
+            pbVida.Value /= 2;
+            if (pbVida.Value < 1)
+            {
+                consolaKO();
+            }
+            else
+            {
+                Storyboard sb = (Storyboard)this.Resources["sbTambor"];
+                sb.Begin();
+            }
+        }
+
+        private void consolaKO()
+        {
+            Storyboard sb = (Storyboard)this.Resources["sbKO"];
+            tbConsola.Text = "¡ SNORLAX se debilitó ! ";
+            sb.Begin();
+            this.imPocion.Opacity = 0.0;
+        }
+
+        private void subirVida(object sender, TappedRoutedEventArgs e)
+        {
+            this.pbVida.Value = 100;
+            this.imPocion.Opacity = 0.0;          
+        }
+     
+    }
+}
